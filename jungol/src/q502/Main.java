873b7589f2409e77cd8a708cb7cc00d0d65948b5
pmsoo <pmsoo@Park>
2024-12-30 16:13:24 +0900

    Initial

--- jungol/src/q502/Main.java
+++ jungol/src/q502/Main.java
@@ -3,6 +3,6 @@ package q502;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Programming! It's fun.");
+		System.out.println("Fun Programming!");
 	}
 }
---
+++ jungol/src/q509/Main.java
@@ -0,0 +1,10 @@
+package q509;
+
+class Main{
+    public static void main(String[] args){
+    	
+        int a = -100;
+        
+        System.out.println(a);
+    }
+}
