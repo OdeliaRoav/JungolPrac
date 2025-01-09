4c9299c492e250adaf19fb252460ebc94f49e4ea
pmsoo <pmsoo4@naver.com>
2025-01-09 17:28:27 +0900

    Initial

---
+++ jungol/src/q1291/Main.java
@@ -0,0 +1,63 @@
+package q1291;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		/*BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());*/
+		
+		while(true) {
+			/*int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());*/
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			if(a>=2 && a<=9 && b>=2 && b<=9) {
+				
+				if(a>b) {
+					
+					for(int i = 1; i<=9; i++) {
+						for(int j = a; j>=b; j--) {
+							System.out.printf("%d * %d =%3d   ", j, i , j*i);
+							}
+						System.out.println();
+						}
+					
+				}
+				else if(b<a) {
+					
+					for(int i = 1; i<=9; i++) {
+						for(int j = b; j>=a; j--) {
+							System.out.printf("%d * %d =%3d   ", j, i , j*i);
+							}
+						System.out.println();
+						}
+					
+				}
+				else {
+					for(int i = 1; i<=9; i++) {
+						System.out.printf("%d * %d =%3d\n", a, i, a*i);
+					}
+				}
+				break;
+				
+			}
+			else {
+				System.out.println("INPUT ERROR!");
+			}
+			
+			
+		}
+		
+		
+	}
+//System.out.println(a + " * " + i + " = "+a*i +"   " +  b + " * " + i + " = " + b*i  );
+}//System.out.printf("%d * %d =%3d   %d * %d =%3d \n", a, i, a*i, b, i, b*i );
---
+++ jungol/src/q514/Main.java
@@ -0,0 +1,22 @@
+package q514;
+
+import java.util.Scanner;	
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("height = ");
+		int a = sc.nextInt();
+		
+		
+		System.out.print("Your height is " + a + "cm.");
+		
+		
+		
+	}
+
+}
