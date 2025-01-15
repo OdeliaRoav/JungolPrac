e360937a3077b070bf456e31f32daae8c5fa3ca8
pmsoo <pmsoo4@naver.com>
2025-01-15 17:46:54 +0900

    Initial

--- jungol/src/q1840/Main.java
+++ jungol/src/q1840/Main.java
@@ -4,120 +4,130 @@ import java.util.Scanner;
 
 public class Main {
 	static int h, w;
-	static int [][] ar, visited;
-	
+	static int[][] ar, visited;
 
 	public static void printArr() {
-		System.out.println("--- Arr ----------------");
-		for(int i = 0; i < ar.length; i++) {
-			for(int j = 0; j<ar[i].length; j++) {
-				System.out.print(ar[i][j]+ " ");				
+		System.out.println("--- Arr ------------------");
+		for (int i = 0; i < ar.length; i++) {
+			for (int j = 0; j < ar[i].length; j++) {
+				System.out.print(ar[i][j] + " ");
 			}
 			System.out.println();
 		}
 	}
-	
+
 	public static void printVisited() {
-		System.out.println("--- Visited ----------------");
-		for(int i = 0; i < ar.length; i++) {
-			for(int j = 0; j<ar[i].length; j++) {
-				System.out.print(visited[i][j]+ " ");				
+		System.out.println("--- Visited ------------------");
+		for (int i = 0; i < ar.length; i++) {
+			for (int j = 0; j < ar[i].length; j++) {
+				System.out.print(visited[i][j] + " ");
 			}
 			System.out.println();
 		}
 	}
-	
+
 	public static boolean checkCheese() {
 		boolean isCheese = false;
-		for(int i = 0; i < ar.length; i++) {
-			for(int j = 0; j<ar[i].length; j++) {
-				if(ar[i][j] == 1) {
+		for (int i = 0; i < ar.length; i++) {
+			for (int j = 0; j < ar[i].length; j++) {
+				if (ar[i][j] == 1) {
 					isCheese = true;
-					
 				}
 			}
-			
 		}
+
 		return isCheese;
 	}
-	
-	public static void meltingCheese() {
-		
-	}
-	
-	private static int countingCheese() {
-		
-	}
-	
-	
+
 	public static void r(int ph, int pw) {
 		visited[ph][pw] = 1;
-		
 		// 탈출 조건
-		if(ar[ph][pw] == 1) {
+		if (ar[ph][pw] == 1) {
 			ar[ph][pw] = 2;
-			return; 
+			return;
 		}
-		
-		// recucrsive 
-		
+
+		// recursive
+
 		// UP
-		if(ph>0 && visited[ph-1][pw] == 0) 
-			r(ph-1, w);
+		if (ph >= 1 && visited[ph - 1][pw] == 0)
+			r(ph - 1, pw);
+
 		// DOWN
-		if(ph<h-1 && visited[ph+1][pw] == 0) 
-			r(h+1, w);
+		if (ph < h - 1 && visited[ph + 1][pw] == 0)
+			r(ph + 1, pw);
+
 		// LEFT
-		if(pw>=1 && visited[ph][pw-1] == 0) 
-			r(h, w-1);
+		if (pw >= 1 && visited[ph][pw - 1] == 0)
+			r(ph, pw - 1);
+
 		// RIGHT
-		if(pw<w-1 && visited[ph][pw+1] == 0) 
-			r(h, w+1);
-		
+		if (pw < w - 1 && visited[ph][pw + 1] == 0)
+			r(ph, pw + 1);
 	}
-	
+
 	public static void initVisited() {
-		for(int i = 0; i < ar.length; i++) {
-			for(int j = 0; j<ar[i].length; j++) {
-				if(ar[i][j] == 1) {
-					
+		for (int i = 0; i < ar.length; i++) {
+			for (int j = 0; j < ar[i].length; j++) {
+				visited[i][j] = 0;
+			}
+		}
+	}
+
+	public static void meltingCheese() {
+		for (int i = 0; i < ar.length; i++) {
+			for (int j = 0; j < ar[i].length; j++) {
+				if (ar[i][j] == 2) {
+					ar[i][j] = 0;
 				}
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		h = sc.nextInt();
 		w = sc.nextInt();
-		
 		ar = new int[h][w];
 		visited = new int[h][w];
-		
-		for(int i = 0; i < ar.length; i++) {
-			for(int j = 0; j<ar[i].length; j++) {
+
+		for (int i = 0; i < ar.length; i++) {
+			for (int j = 0; j < ar[i].length; j++) {
 				ar[i][j] = sc.nextInt();
-				
 			}
 		}
 		sc.close();
-		
+
 		int time = 0;
 		int cCheese = 0;
 		do {
 			time++;
 			initVisited();
-			
-			printArr();
-			printVisited();
-			
-			r(0,0);
+
+//			printArr();
+//			printVisited();
+
+			cCheese = countingCheese();
+			r(0, 0);
 			meltingCheese();
 			
-			
-			
-		}while(checkCheese());
-		
+//			printArr();
+//			printVisited();
+
+		} while (checkCheese());
+		System.out.println(time);
+		System.out.println(cCheese);
 	}
 
+	private static int countingCheese() {
+		int c = 0;
+		for (int i = 0; i < ar.length; i++) {
+			for (int j = 0; j < ar[i].length; j++) {
+				if (ar[i][j] == 1) {
+					c++;
+				}
+			}
+		}
+		return c;
+	}
 }
