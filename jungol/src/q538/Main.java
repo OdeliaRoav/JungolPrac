b509ea03209bcf6ff7b402fff88c0c41347e5426
pmsoo <pmsoo4@naver.com>
2025-01-17 16:30:15 +0900

    Initial

---
+++ jungol/src/q538/Main.java
@@ -0,0 +1,31 @@
+package q538;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);	
+		
+		
+		
+		while(true) {
+			System.out.print("number? ");
+			int a = sc.nextInt();
+			
+			if(a<0) {
+				System.out.println("negative number");
+			}
+			else if(a>0) {
+				System.out.println("positive integer");
+			}
+			else {
+				break;
+			}
+			
+			
+		}
+		
+	}
+
+}
