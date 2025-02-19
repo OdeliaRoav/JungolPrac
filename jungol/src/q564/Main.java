c7666bf6f90a8ff6a4fd3b4e8efc5831f6c0c76f
pmsoo <pmsoo4@naver.com>
2025-02-19 14:14:16 +0900

    Initial

--- jungol/src/q564/Main.java
+++ jungol/src/q564/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int[] count = new int[26]; // 알파벳 개수 저장 배열
 
-        while (sc.hasNext()) {
+        while (true) { //hasNext()는 boolean으로 True or False를 리턴함 그래서 그냥 sc.hasNext()하면 True 반환(False 값 나오기 전까지)
             char ch = sc.next().charAt(0); // 한 글자 입력받기
             if (ch < 'A' || ch > 'Z') break; // 대문자가 아니면 입력 종료
             count[ch - 'A']++; // 해당 문자 카운트 증가
@@ -18,7 +18,7 @@ public class Main {
         
         for (int i = 0; i < 26; i++) {
             if (count[i] > 0) { // 1번 이상 등장한 문자만 출력
-                System.out.println((char) (i + 'A') + " " + count[i]);
+                System.out.println((char) (i + 'A') + " : " + count[i]);
             }
         }
     }
