73bb91961b4a8f92c8bfd2b6ebcb4e3b6f121dc9
pmsoo <pmsoo4@naver.com>
2025-02-19 14:03:28 +0900

    Initial

--- jungol/src/q618/Main.java
+++ jungol/src/q618/Main.java
@@ -1,9 +1,11 @@
 package q618;
 
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
 
-class info {
-    String Name;
+class Info {
+    String name;
     int height;
     float weight;
 }
@@ -11,38 +13,33 @@ class info {
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        info[] a = new info[5];
-
-        for (int i = 0; i < 5; i++) {
-            a[i] = new info();
-        }
+        Info[] people = new Info[5];
 
         // 입력 받기
         for (int i = 0; i < 5; i++) {
-            a[i].Name = sc.next();
-            a[i].height = sc.nextInt();
-            a[i].weight = sc.nextFloat();
+            people[i] = new Info();
+            people[i].name = sc.next(); // 이름 (최대 20자)
+            people[i].height = sc.nextInt();
+            people[i].weight = sc.nextFloat();
         }
         
         sc.close();
 
-        // 버블 정렬을 이용한 weight 기준 내림차순 정렬
-        for (int i = 0; i < 5 - 1; i++) {
-            for (int j = 0; j < 5 - 1 - i; j++) {
-                if (a[j].weight < a[j + 1].weight) {
-                    // 객체 교환 (swap)
-                    info temp = a[j];
-                    a[j] = a[j + 1];
-                    a[j + 1] = temp;
-                }
-            }
-        }
+        // 1. 이름순 정렬 (오름차순)
+        Arrays.sort(people, Comparator.comparing(o -> o.name));
 
-        // 정렬된 결과 출력
+        System.out.println("name");
+        for (Info person : people) {
+            System.out.printf("%s %d %.1f\n", person.name, person.height, person.weight);
+        }
+        
+        System.out.println();
+        // 2. 몸무게순 정렬 (내림차순)
+        Arrays.sort(people, Comparator.comparingDouble((Info o) -> o.weight).reversed());
+
         System.out.println("weight");
-        for (int i = 0; i < 5; i++) {
-            System.out.println(a[i].Name + " " + a[i].height + " " + a[i].weight);
+        for (Info person : people) {
+            System.out.printf("%s %d %.1f\n", person.name, person.height, person.weight);
         }
     }
 }
