31428325983e98a833493d3a122359ee286f3ab2
pmsoo <pmsoo4@naver.com>
2025-02-20 14:28:57 +0900

    Initial

--- jungol/src/q565/Main.java
+++ jungol/src/q565/Main.java
@@ -7,10 +7,23 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		
+		
 		while(true) {
 			int a = sc.nextInt();
-		
-		
+			
+			if(a == 0) {
+				break;
+			}
+			else {
+				if( a%10 == 0) {
+					
+				}
+				else {
+					
+				}
+			}
+			
 		}
 	
 	}
