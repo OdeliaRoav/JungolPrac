595f3c136e235f5d000295ddf10dd275d6851e64
pmsoo <pmsoo4@naver.com>
2025-02-20 14:31:08 +0900

    Initial

--- jungol/src/q565/Main.java
+++ jungol/src/q565/Main.java
@@ -3,29 +3,23 @@ package q565;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] count = new int[26]; // 알파벳 개수 저장 배열
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		
-		
-		while(true) {
-			int a = sc.nextInt();
-			
-			if(a == 0) {
-				break;
-			}
-			else {
-				if( a%10 == 0) {
-					
-				}
-				else {
-					
-				}
-			}
-			
-		}
-	
-	}
+        while (true) { //hasNext()는 boolean으로 True or False를 리턴함 그래서 그냥 sc.hasNext()하면 True 반환(False 값 나오기 전까지)
+            char ch = sc.next().charAt(0); // 한 글자 입력받기
+            if (ch < 'A' || ch > 'Z') break; // 대문자가 아니면 입력 종료
+            count[ch - 'A']++; // 해당 문자 카운트 증가
+        }
 
+        sc.close();
+
+        
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > 0) { // 1번 이상 등장한 문자만 출력
+                System.out.println((char) (i + 'A') + " : " + count[i]);
+            }
+        }
+    }
 }
