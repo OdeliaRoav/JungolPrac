c5f8815b7fd796f415a02dfce0729ae2374dff73
pmsoo <pmsoo4@naver.com>
2025-02-28 10:54:38 +0900

    Initial

---
+++ jungol/src/q607/Main.java
@@ -0,0 +1,25 @@
+package q607;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		String a = sc.next();
+		String b = sc.next();
+		
+		String c = a.substring(0,2);
+		
+		
+		System.out.print(c);
+		
+		for(int i = 0; i<b.length()-2; i++) {
+			System.out.print(b.charAt(i+2));
+		}
+		System.out.print(c);
+	}
+
+}
