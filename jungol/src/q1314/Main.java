a953ce65ff072c62c0e1a3a7a5ff792f86abb483
pmsoo <pmsoo4@naver.com>
2025-03-06 17:29:47 +0900

    Initial

--- jungol/src/q1314/Main.java
+++ jungol/src/q1314/Main.java
@@ -3,25 +3,35 @@ package q1314;
 import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		char ch = 'A';
-		
-		int n = sc.nextInt();
-		
-		for(int j = 0; j<n; j++) {
-			ch = (char) ('A' + j);
-			for(int i = 1; i<=n; i++) {
-				System.out.print(ch+" ");
-				ch+=n;
-			}
-			System.out.println();
-			
-		}
-		
-		
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int n = sc.nextInt();
+ 
+        char[][] charArr = new char[n][n];
+         
+        char start = 'A';
+         
+        for(int i = 0; i < n; i++) {
+            if(i % 2 == 0) {
+                for(int j = 0; j < n; j++) {
+                    if(start > 'Z') start = 'A';
+                    charArr[j][i] = start++;
+                }
+            }else {
+                for(int j = n-1; j >= 0; j--) {
+                    if(start > 'Z') start = 'A';
+                    charArr[j][i] = start++;
+                }
+            }
+        }
+         
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print(charArr[i][j] + " ");
+            }
+            System.out.println();
+        }
+         
+    }
 }
