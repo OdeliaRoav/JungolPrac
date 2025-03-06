52510b7a8f6d889dd241c7ebcfc0424e217c118e
pmsoo <pmsoo4@naver.com>
2025-03-06 09:50:12 +0900

    Initial

--- jungol/src/q563/Main.java
+++ jungol/src/q563/Main.java
@@ -16,17 +16,16 @@ public class Main {
 			Array[i] = sc.nextInt();
 		}
 		
-		Arrays.sort(Array);
+		//Arrays.sort(Array);
 		
-		for(int i = 0; i<Array.length; i++) {
-			for(int j = i+1; j<Array.length; j++) {
-				int num = Array[i];
-				
-				if(num<Array[j]) {
-					num = Array[j];
+		for(int i = 0; i<Array.length-1; i++) {
+			for(int j = 0; j<Array.length -i -1; j++) {
+				if(Array[j]>Array[j+1]) {
+					
+					int num = Array[j];
+					Array[j] = Array[j+1];
+					Array[j+1] = num;
 				}
-				
-				Array2[i] = num;
 				
 			}
 		}
