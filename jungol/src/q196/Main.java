4099ba8b810c2fdf53fc083ca890eac1104285b4
pmsoo <pmsoo4@naver.com>
2025-03-13 09:39:11 +0900

    Initial

--- jungol/src/q196/Main.java
+++ jungol/src/q196/Main.java
@@ -1,30 +1,39 @@
 package q196;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-class Info {
+class Info implements Comparable<Info> {
     String name;
-    int number;
-    String Address;
+    String number;
+    String address;  // 변수명 수정 (일반적으로 소문자로 시작)
+
+    @Override
+    public int compareTo(Info other) {
+        return this.name.compareTo(other.name);  // 이름 기준 사전순 정렬
+    }
 }
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		 Info[] people = new Info[3];
-		 
-		 for(int i = 0; i<3; i++) {
-			 people[i] = new Info();
-			 people[i].name = sc.next();
-			 people[i].number = sc.nextInt();
-			 people[i].Address = sc.next();
-			 
-		 }
+        Info[] people = new Info[3];
 
-		
-	}
+        // 입력 받기
+        for (int i = 0; i < 3; i++) {
+            people[i] = new Info();
+            people[i].name = sc.next();
+            people[i].number = sc.next();
+            people[i].address = sc.next();
+        }
+        
+        // 정렬 (이름 기준)
+        Arrays.sort(people);  
 
+        System.out.println("name : " + people[0].name);
+        System.out.println("tel : " + people[0].number);
+        System.out.println("addr : " + people[0].address);
+        
+       }
 }
