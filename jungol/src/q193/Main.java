9e7cbd4087734ca03e868bbd8d648c5ff27509bb
pmsoo <pmsoo4@naver.com>
2025-03-14 09:24:07 +0900

    Initial

---
+++ jungol/src/q193/Main.java
@@ -0,0 +1,27 @@
+package q193;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String Array[] = new String[5];
+		
+		for(int i = 0; i<5; i++) {
+			Array[i] = sc.next();
+		}
+		String a = sc.next();
+		char d = a.charAt(0);
+		String b = sc.next();
+		
+		for(int i = 0; i<5; i++) {
+			for(int j = 0; j<Array[i].length(); j++ ) {
+				if()
+			}
+		}
+		
+	}
+
+}
