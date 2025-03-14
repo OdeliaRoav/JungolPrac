f3e975f396801d4410f14bafb01d92cc390f4bae
pmsoo <pmsoo4@naver.com>
2025-03-14 09:35:39 +0900

    Initial

--- jungol/src/q193/Main.java
+++ jungol/src/q193/Main.java
@@ -3,25 +3,34 @@ package q193;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		String Array[] = new String[5];
-		
-		for(int i = 0; i<5; i++) {
-			Array[i] = sc.next();
-		}
-		String a = sc.next();
-		char d = a.charAt(0);
-		String b = sc.next();
-		
-		for(int i = 0; i<5; i++) {
-			for(int j = 0; j<Array[i].length(); j++ ) {
-				if()
-			}
-		}
-		
-	}
+        // 5개의 단어 입력 받기
+        String[] Array = new String[5];
+        for (int i = 0; i < 5; i++) {
+            Array[i] = sc.next();
+        }
 
+        // 검색할 문자 및 단어 입력 받기
+        char d = sc.next().charAt(0);
+        String b = sc.next();
+
+        boolean found = false;
+
+        // 특정 문자를 포함하는 단어 출력
+        for (String word : Array) {
+            if (word.indexOf(d) != -1 || word.contains(b)) {
+                System.out.println(word);
+                found = true;
+            }
+        }
+
+        // 아무것도 출력되지 않았다면 "none" 출력
+        if (!found) {
+            System.out.println("none");
+        }
+
+        sc.close();
+    }
 }
