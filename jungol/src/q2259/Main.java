b82e0806ed3aee70b44af0fdf846068faedd5ab0
pmsoo <pmsoo4@naver.com>
2025-03-27 09:26:49 +0900

    Initial

---
+++ jungol/src/q2259/Main.java
@@ -0,0 +1,33 @@
+package q2259;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		/*
+		switch(a) {
+		case 4:
+			break;
+		case 3:
+			break;
+		case 2:
+			break;
+		case 1:
+			break;
+		}
+		*/
+		
+	}
+
+}
+/*
+4 = 북
+3 = 남
+2 = 서
+1 = 동
+*/
